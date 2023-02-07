38804a503018c3eb29e6ca687dfc632bc9e49641
shyambandla <shyambandla143@gmail.com>
2023-02-07 23:36:22 +0530

    Update VehicleImagesScreen.js

--- src/screens/VehicleImagesScreen.js
+++ src/screens/VehicleImagesScreen.js
@@ -35,6 +35,7 @@ const text = str;
 const result = text.replace(/([A-Z])/g, " $1");
 const finalResult = result.charAt(0).toUpperCase() + result.slice(1);
 console.log(finalResult);
+return finalResult;
     }
 
 
